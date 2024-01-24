ed0112771e3a2dd8e02f708fa12a08e6b4f4f0bf
K L PRANAAV REDDY <99110613+klpranaav@users.noreply.github.com>
2024-01-24 23:37:14 +0530

    Update model_builder.py

--- going_modular/going_modular/model_builder.py
+++ going_modular/going_modular/model_builder.py
@@ -18,8 +18,8 @@ class TinyVGG(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
         super().__init__()
         self.conv_block_1 = nn.Sequential(
-          nn.Conv2d(in_channels=input_shape, 
-                    out_channels=hidden_units, 
+          nn.Conv2d(in_channels=1, 
+                    out_channels=1, 
                     kernel_size=3, 
                     stride=1, 
                     padding=0),  
