cdad0ae6c1a61e6ea324de568faf193982ca3847
K L PRANAAV REDDY <99110613+klpranaav@users.noreply.github.com>
2024-01-26 17:26:01 +0530

    Update predictions.py

--- going_modular/going_modular/predictions.py
+++ going_modular/going_modular/predictions.py
@@ -47,9 +47,9 @@ def pred_and_plot_image(
             [
                 transforms.Resize(image_size),
                 transforms.ToTensor(),
-                transforms.Normalize(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                ),
+                # transforms.Normalize(
+                #     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                # ),
             ]
         )
 
